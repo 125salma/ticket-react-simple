6267f27620a17507a78c50c43f676eca0bb91008
winmiraj <winmiraj@gmail.com>
2024-05-30 00:38:55 +0600

    first commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className='header'> 
+           <div className='header-margin'>
+            <h2>Online Bus Ticket Sales</h2>
+            <p>You can buy a bus ticket online system in a simple way</p>
+            <h3>Total Budget: 100 Million</h3>
+           </div>
+           
+        </div>
+    );
+};
+
+export default Header;
